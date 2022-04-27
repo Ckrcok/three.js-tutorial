8e920e09c66770a6483974e87ef2e9952e2f969f
Dor <dorzairi@ymail.com>
2022-04-27 14:11:26 -0400

    added a plane and a box to a simple scene

--- chapter-01/01_03/begin/main.js
+++ chapter-01/01_03/begin/main.js
@@ -1,1 +1,63 @@
-console.log(THREE);
+
+
+function init(){
+    var scene = new THREE.Scene();
+
+    let box = getBox(1, 1, 1);
+    let plane = getPlane(4);
+
+    box.position.y = box.geometry.parameters.height/2;
+    plane.rotation.x = Math.PI/2;
+
+    scene.add(box);
+    scene.add(plane);
+
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 100);
+
+    
+    
+    camera.position.x = 1;
+    camera.position.y = 2;
+    camera.position.z = 5;
+
+    camera.lookAt(new THREE.Vector3(0,0,0));
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('webgl').appendChild(renderer.domElement);
+    renderer.render(scene,camera);
+}
+
+function getBox(w, h, d){
+    let geometry = new THREE.BoxGeometry(w, h, d );
+    let material =   new THREE.MeshBasicMaterial({
+        color : 0x00ff00
+    });
+
+    let mesh = new THREE.Mesh(
+        geometry,
+        material
+    );
+
+    return mesh;
+}
+
+
+function getPlane(size){
+    let geometry = new THREE.PlaneGeometry(size, size);
+    let material =   new THREE.MeshBasicMaterial({
+        color : 0xff0000 , 
+        side : THREE.DoubleSide
+    });
+
+    let mesh = new THREE.Mesh(
+        geometry,
+        material
+    );
+
+    return mesh;
+}
+
+
+init();
+
