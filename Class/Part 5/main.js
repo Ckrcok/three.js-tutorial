deea2ec15ff3bea08e9758a7c5d8f7ffd0033956
Dor <dorzairi@ymail.com>
2022-04-28 19:52:51 -0400

    added a partical shpere

--- Class/Part 5/main.js
+++ Class/Part 5/main.js
@@ -8,34 +8,30 @@ function init() {
 		1, // near clipping plane
 		1000 // far clipping plane
 	);
-	camera.position.z = 0;
+	camera.position.z = 30;
 	camera.position.x = 0;
 	camera.position.y = 1;
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 
-	let particaleGeo = new THREE.Geometry();
+
 	let particaleMaterial = new THREE.PointsMaterial({
 		color:'rgb(255,255,255)',
-		size: 1 ,
+		size: 0.25 ,
 		map: new THREE.TextureLoader().load('assets/textures/particle.jpg'),
 		transparent: true,
 		blending : THREE.AdditiveBlending ,
 		depthWrite : false,
 	});
-	
-	let particalCount = 20000;
-	let particalDistance = 100;
 
+	let particaleGeo = new THREE.SphereGeometry(10, 64, 64);
 
-	for (let i = 0; i < particalCount ; i++) {
-		let posX = (Math.random()-0.5) * particalDistance;
-		let posY= (Math.random()-0.5) * particalDistance;
-		let posZ = (Math.random()-0.5) * particalDistance;
-		let partical = new THREE.Vector3(posX, posY, posZ);
+	particaleGeo.vertices.forEach(vertex => {
+		vertex.x += (Math.random()- 0.5);
+		vertex.y += (Math.random()- 0.5);
+		vertex.z += (Math.random()- 0.5);
+	});
 
-		particaleGeo.vertices.push(partical);
-	}
 
 	let particalSystem = new THREE.Points(
 		particaleGeo,
@@ -67,25 +63,8 @@ function update(renderer, scene, camera, controls) {
 	renderer.render(scene, camera);
 	
 	let particalSystem =  scene.getObjectByName('particalSystem');
-	particalSystem.rotation.z += 0.0009;
-	particalSystem.geometry.vertices.forEach(particle => {
-		particle.x += (Math.random()-1) * 0.1 ;
-		particle.y += (Math.random()- 0.75)  * 0.1 ;
-		particle.z += (Math.random()) * 0.1 ;
 
-		if (particle.x < -50) {
-			particle.x = 50;
-		}
-		if (particle.y < -50) {
-			particle.y = 50;
-		}
-		if (particle.z < -50) {
-			particle.z = 50;
-		}
-		if (particle.z > 50) {
-			particle.z = -50;
-		}
-	})
+	particalSystem.rotation.y += 0.0005;
 
 	particalSystem.geometry.verticesNeedUpdate = true;
 
